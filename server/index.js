3d8d5eff77d1ee19e92d9d2070e42eb3c7ecbf66
Sam-Park <sam.park.email@gmail.com>
2018-09-01 22:58:58 -0400

    just seeing if it works

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
+  res.send({ hi: 'there v2.0'});
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
